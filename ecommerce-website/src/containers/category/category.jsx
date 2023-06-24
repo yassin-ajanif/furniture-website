2ed352ff32357ae3f1ecdff6058bf46d8e7108c8
yassin <yassin.ajanif@gmail.com>
2023-06-24 23:57:14 +0200

    making a content scrolable

--- ecommerce-website/src/containers/category/category.jsx
+++ ecommerce-website/src/containers/category/category.jsx
@@ -12,39 +12,40 @@ import arrowUp from "../category/category-assets/arrow2.png";
 import shortLine from "../category/category-assets/shorter-line.png";
 import longerLine from "../category/category-assets/longer-line.png";
 
-import bedroom from '../category/category-assets/bedroom-resize.jpg'
-import dinning_room from '../category/category-assets/dinning room.jpg'
-import meeting_room from '../category/category-assets/meeting room.jpg'
-import workspace from '../category/category-assets/workspace.jpg'
-import living_room from '../category/category-assets/living room.jpg'
-import kitechen_room from '../category/category-assets/kitechen room.jpg'
+import bedroom from "../category/category-assets/bedroom-resize.jpg";
+import dinning_room from "../category/category-assets/dinning room.jpg";
+import meeting_room from "../category/category-assets/meeting room.jpg";
+import workspace from "../category/category-assets/workspace.jpg";
+import living_room from "../category/category-assets/living room.jpg";
+import kitechen_room from "../category/category-assets/kitechen room.jpg";
 
 const Category = () => {
+  const maxScrollDown = 900;
+  const maxScrollUp = 0;
+  const oneStepScroll = 100;
+  const [scrollPosition, setscrollPosition] = useState(0);
+  const [value,setValue]=useState(0)
 
-  const maxScrollDown=600;
-  const maxScrollUp=0;
-  const [scrollingUpBy,setscrollingUpBy]=useState(0);
-  const [scrollingDownBy,setscrollingDownBy]=useState(0);
-  const [scrollPosition,setscrollPosition]=useState(0)
-
-  console.log(scrollingDownBy)
-
-function down(){
-
-   scrollPosition === maxScrollDown ? '' : setscrollPosition(scrollPosition+100)
-
+  function down() {
+    
+    if(scrollPosition !== maxScrollDown)  
+    {   setscrollPosition(scrollPosition + oneStepScroll);
+        setValue(value+1)}
+       
   }
-function up(){
+  function up() {
 
-  scrollPosition === maxScrollUp ? '' : setscrollPosition(scrollPosition-100)
-
-}
+    if(scrollPosition !== maxScrollUp)
+     
+    {setscrollPosition(scrollPosition - oneStepScroll);
+        setValue(value-1)}
+      
+    }
 
   return (
     <div className="category">
       <h2 className="category-title">Explore by Category</h2>
       <div className="category-context">
-
         <div className="category-sidebar">
           <div className="category-searchBar-mobileIcon">
             <div className="serach-category">
@@ -60,76 +61,51 @@ function up(){
           </div>
 
           <div className="category-links">
+            <div className="category-links-top-bottom">
+              <div className="category-links-top">
+                <NavLink to="/az">Bedroom</NavLink>
+                <NavLink to="/">Dinning Room</NavLink>
+                <NavLink to="/">Meeting Room</NavLink>
+                <NavLink to="/">Workspace</NavLink>
+              </div>
 
-          <div className="category-links-top-bottom">
-
-            <div className="category-links-top">
-              <NavLink
-                to="/az"
-                
-              >
-                Bedroom
-              </NavLink>
-              <NavLink
-                to="/"
-                
-              >
-                Dinning Room
-              </NavLink>
-              <NavLink
-                to="/"
-                
-              >
-                Meeting Room
-              </NavLink>
-              <NavLink
-                to="/"
-                
-              >
-                Workspace
-              </NavLink>
+              <div className="category-links-bottom">
+                <NavLink to="/">LivingRoom</NavLink>
+                <NavLink to="/">Room Kitchen</NavLink>
+                <NavLink to="/">Living Space</NavLink>
+              </div>
             </div>
 
-            <div className="category-links-bottom">
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "" : "")}
-              >
-                LivingRoom
-              </NavLink>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "" : "")}
-              >
-                Room Kitchen
-              </NavLink>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "" : "")}
-              >
-                Living Space
-              </NavLink>
+            <div className="category-links-desktop" style={{translate : ` 0 -${value*14.28}% `}}>
+
+            <div className="shown-links"> 
+              <NavLink to="/az">Bedroom</NavLink>
+              <NavLink to="/">Dinning Room</NavLink>
+              <NavLink to="/">Meeting Room</NavLink>
+              <NavLink to="/">Workspace</NavLink>
+              <NavLink to="/">LivingRoom</NavLink>
+              <NavLink to="/">Room Kitchen</NavLink>
+              <NavLink to="/">Living Space</NavLink>
             </div>
-
-            <div className="category-links-hidden">
-              <div className="test1">test1</div>
-              <div className="test2">test2</div>
-              <div className="test3">test3</div>
+            <div className="hidden-links">
+              <NavLink to="/">Test 1</NavLink>
+              <NavLink to="/">Test 2</NavLink>
+              <NavLink to="/">Test 3</NavLink>
             </div>
-
-          </div>
-
+      </div>
             <div className="category-sidebar-scrollBar">
-              
               <div className="scrollBar-line">
                 <div className="longer-line">
                   <img src={longerLine} alt="" />
                 </div>
-                <div className="shorter-line" style={{ translate : ` 0 ${scrollPosition}%` }}>
+                <div
+                  className="shorter-line"
+                  style={{ translate: ` 0 ${scrollPosition}%` }}
+                >
                   <img src={shortLine} alt="" />
                 </div>
               </div>
-              <div className="circle-up" onClick={()=>up()}>
+              <div className="circle-up" onClick={() => up()}>
                 <div className="up-circle">
                   <img src={upCircle} alt="" />
                 </div>
@@ -138,7 +114,7 @@ function up(){
                 </div>
               </div>
 
-              <div className="circle-down" onClick={()=>down()}>
+              <div className="circle-down" onClick={() => down()}>
                 <div className="down-circle">
                   <img src={donwCirle} alt="" />
                 </div>
@@ -148,9 +124,8 @@ function up(){
                 </div>
               </div>
             </div>
-
           </div>
-          
+
           <button className="allCategory-btn tableDesktop-display">
             <div>All Categories</div>
             <img src={arrow} alt="" />
@@ -158,16 +133,16 @@ function up(){
         </div>
 
         <div className="category-images">
-
-        <CategoryImg image={bedroom} name={"Bedroom"} link={"/y"} />
+          <CategoryImg image={bedroom} name={"Bedroom"} link={"/y"} />
           <CategoryImg image={dinning_room} name={"Dinning Room"} link={"/"} />
           <CategoryImg image={meeting_room} name={"Meeting Room"} link={"/b"} />
           <CategoryImg image={workspace} name={"Workspace"} link={"/c"} />
           <CategoryImg image={living_room} name={"Living Room"} link={"/e"} />
-  <CategoryImg image={kitechen_room} name={"Kitchen Room"} link={"/f"} /> 
-
-
-
+          <CategoryImg
+            image={kitechen_room}
+            name={"Kitchen Room"}
+            link={"/f"}
+          />
         </div>
       </div>
 
