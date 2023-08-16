f0aec9ea9a8c05d5bc9fe613d87af7513fd0d7ba
yassin <yassin.ajanif@gmail.com>
2023-08-16 11:06:43 +0200

    still fixing case sentivity issues

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ export { default as Footer } from '../src/containers/footer/footer'
 
 export { default as Bedrooms } from '../src/containers/products/Bedrooms/Bedrooms'
 export { default as DinningRooms } from '../src/containers/products/DinningRooms/DinningRooms'
-export { default as LivingRooms } from '../src/containers/products/livingRomms/LivingRooms'
-export { default as MeetingRooms } from '../src/containers/products/meetingRooms/MeetingRooms'
-export { default as RoomKitchens } from '../src/containers/products/rommKitchens/RoomKitchens'
-export { default as Workspaces } from '../src/containers/products/workspaces/workspaces'
+export { default as LivingRooms } from '../src/containers/products/LivingRomms/LivingRooms'
+export { default as MeetingRooms } from '../src/containers/products/MeetingRooms/MeetingRooms'
+export { default as RoomKitchens } from '../src/containers/products/RommKitchens/RoomKitchens'
+export { default as Workspaces } from '../src/containers/products/Workspaces/workspaces'
 
 export { default as AllProducts } from '../src/containers/allProducts/AllProducts'
 
